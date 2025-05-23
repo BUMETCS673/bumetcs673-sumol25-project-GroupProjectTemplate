0f6a1aa7576f850854f5dc23f78b8bc5552c1047
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-05-22 20:45:26 -0400

    Create SignUp.jsx

---
+++ code/frontend/src/pages/SighUp/SignUp.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./SignUp.css";
+import SignInImg from "../../assets/signin_image.png";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+
+const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <section className="SignUp">
+      <div className="signup-header">
+        <h1 className="signup-h1">MY MAGICAL BEDTIME</h1>
+      </div>
+
+      <div className="signup-right">
+        <div className="signup-box">
+          <h2>Sign Up</h2>
+          <form className="signup-form">
+            <input
+              type="text"
+              placeholder="First Name"
+              className="signup-input"
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              className="signup-input"
+            />
+            <input type="email" placeholder="Email" className="signup-input" />
+            <div className="password-wrapper">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                className="signup-input"
+              />
+              <span
+                className="toggle-icon"
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+              </span>
+            </div>
+            <button type="submit" className="signup-submit-btn">
+              SIGN UP
+            </button>
+          </form>
+          <p className="signup-text">
+            Already have an account? <Link to="/login">Sign in</Link>
+          </p>
+          <div className="divider">
+            <span></span>
+            <p>OR</p>
+            <span></span>
+          </div>
+          <button className="google-btn">
+            <FontAwesomeIcon icon={faGoogle} className="icon" />
+            Continue with Google
+          </button>
+        </div>
+      </div>
+
+      <div className="signup-bottom-image">
+        <img src={SignInImg} alt="Illustration" className="signup-image" />
+      </div>
+    </section>
+  );
+};
+
+export default SignUp;
