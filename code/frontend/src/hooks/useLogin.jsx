aea473bc4b5f9c6aa64134af965c258164847124
hongjie <hong.zhang94@gmail.com>
2025-05-26 21:33:29 -0400

    fix: update base_url configuration

--- code/frontend/src/hooks/useLogin.jsx
+++ code/frontend/src/hooks/useLogin.jsx
@@ -10,7 +10,7 @@ export const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const BASE_URL = "http://localhost:5500"
+    const BASE_URL = "https://mymagicalbedtime-25abceb2c11f.herokuapp.com"
     const response = await fetch(`${BASE_URL}/api/user/login`, { 
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
