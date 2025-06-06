3238715ef11f4b9c1fd3a326844cf134423f1067
HONG JIE ZHANG <70243495+hongjie94@users.noreply.github.com>
2025-06-06 10:31:51 -0400

    Update main.jsx

--- code/frontend/src/main.jsx
+++ code/frontend/src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -18,22 +17,3 @@ createRoot(document.getElementById("root")).render(
     </AuthContextProvider>
   </StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { AuthContextProvider } from './context/AuthContext.jsx'
-import {GoogleOAuthProvider} from '@react-oauth/google';
-
-createRoot(document.getElementById('root')).render(
-
-  <StrictMode>
-    <AuthContextProvider>
-      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-        <App />
-      </GoogleOAuthProvider>
-    </AuthContextProvider>
-  </StrictMode>,
-)
->>>>>>> origin/main
