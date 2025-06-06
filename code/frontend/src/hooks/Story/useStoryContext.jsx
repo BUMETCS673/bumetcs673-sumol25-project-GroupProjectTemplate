732b0e1d6d02fbabc62326eea788d1546a978fb9
hongjie <hong.zhang94@gmail.com>
2025-06-06 11:15:42 -0400

    update useStoryContext.jsx

--- code/frontend/src/hooks/Story/useStoryContext.jsx
+++ code/frontend/src/hooks/Story/useStoryContext.jsx
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
 import { useContext } from "react";
 import { StoryContext } from "../../context/StoryContext";
-=======
-import StoryContext from '../../context/StoryContext';
-import { useContext } from 'react';
->>>>>>> origin/main
 
 export const useStoryContext = () => {
   const context = useContext(StoryContext);
 
   if (!context) {
-<<<<<<< HEAD
     throw new Error("useStoryContext must be used within a StoryContextProvider");
   }
 
   return context;
 };
-=======
-    throw Error('useStoryContext must be used inside a StoryContextProvider');
-  }
-
-  return context;
-}
-
->>>>>>> origin/main
