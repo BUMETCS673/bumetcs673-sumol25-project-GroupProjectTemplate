c3b9e71c4f5516fa6437c43e8a0e4d3648e02b55
uzayisinalici <isinalc@gmail.com>
2025-06-13 17:33:20 +0300

    Created HIPAA-compliant privacy policy page

--- code/mymedic/users/urls.py
+++ code/mymedic/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('profile', views.profile, name='profile'),
     path('mlogout', views.mlogout, name='mlogout'),
     path('', views.mlogin, name=''),  # Default route to login
+    path("privacy/", views.privacy_policy, name="privacy_policy"),
 ]
